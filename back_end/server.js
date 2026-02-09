99f4637f169de25b5004384506d7b398b2e17312
Vishal Maurya <vishalmaurya8459@gmail.com>
2026-02-09 15:14:42 +0530

    code fix

--- back_end/server.js
+++ back_end/server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/connnectDB");
-const app = express();
-const userRoutes = require("./routes/userRoutes");
-const blogRoutes = require("./routes/blogRoutes");
 const cloudinaryConfig = require("./config/cloudinaryConfig");
 require("dotenv").config();
 
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "https://my-blog-app-five-alpha.vercel.app",
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: "https://my-blog-app-five-alpha.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
+// ✅ SAME config everywhere
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
