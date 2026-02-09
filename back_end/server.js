9e711b78d921608c2b57b632976b58a97d3c574e
Vishal Maurya <vishalmaurya8459@gmail.com>
2026-02-09 18:54:22 +0530

    Fix CORS preflight issue

--- back_end/server.js
+++ back_end/server.js
@@ -13,6 +13,8 @@ app.use(
   cors({
     origin: process.env.ORIGIN_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
