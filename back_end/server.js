b097b78d5f638df5f69ac01770d129e8f69b87d9
Vishal Maurya <vishalmaurya8459@gmail.com>
2026-02-09 15:52:45 +0530

    Fix CORS preflight issue

--- back_end/server.js
+++ back_end/server.js
@@ -1,30 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/connnectDB");
-const cloudinaryConfig = require("./config/cloudinaryConfig");
-require("dotenv").config();
-
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const blogRoutes = require("./routes/blogRoutes");
+const cloudinaryConfig = require("./config/cloudinaryConfig");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "https://my-blog-app-five-alpha.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions)); // ✅ enough for CORS + preflight
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", blogRoutes);
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`server running...${PORT}`);
   connectDB();
   cloudinaryConfig();
-});
-   
+}); 
